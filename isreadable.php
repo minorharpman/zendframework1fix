ddd0e8d3cd55e878fd14c3ce3b197254c0cff4f3
Kunsági Zsolt <kunsagi.zsolt2@telekom.hu>
2019-12-05 14:49:18 +0100

    1

---
+++ isreadable.php
@@ -0,0 +1,46 @@
+     <?php
+    /**
+     * Returns TRUE if the $filename is readable, or FALSE otherwise.
+     * This function uses the PHP include_path, where PHP's is_readable()
+     * does not.
+     *
+     * Note from ZF-2900:
+     * If you use custom error handler, please check whether return value
+     *  from error_reporting() is zero or not.
+     * At mark of fopen() can not suppress warning if the handler is used.
+     *
+     * @param string   $filename
+     * @return boolean
+     */
+    public static function isReadable($filename)
+    {
+        if (is_readable($filename)) {
+            // Return early if the filename is readable without needing the 
+            // include_path
+            return true;
+        }
+
+        if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN'
+            && preg_match('/^[a-z]:/i', $filename)
+        ) {
+            // If on windows, and path provided is clearly an absolute path, 
+            // return false immediately
+            return false;
+        }
+
+        if(strpos($filename, '/')==0) return false; //if absolute path skip
+
+        foreach (self::explodeIncludePath() as $path) {
+            if ($path == '.') {
+                if (is_readable($filename)) {
+                    return true;
+                }
+                continue;
+            }
+            $file = $path . '/' . $filename;
+            if (is_readable(realpath($file))) {
+                return true;
+            }
+        }
+        return false;
+    }
